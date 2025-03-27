80cba34d6d0245d2aef069eeaefb46a1f6dd265e
JhonHCanoB <jhon.canob@campusucc.edu.co>
2025-03-27 20:12:42 +0000

    while loops

---
+++ EstructurasRepeticion/Ejercicio1.py
@@ -0,0 +1,21 @@
+n = 0
+total = 0
+numero = 0
+suma = 0
+
+#pedir n (cantidad de números) por teclado
+n = int(input("Cantidad de números:"))
+total = n
+while(total > 0):
+    numero = int(input("Digite un número:"))
+    suma += numero # suma = suma + numero
+    total -= 1 #total = total - 1
+
+#forma_1
+print("1. La suma de los " , str(n) , " números es: " , str(suma))
+#forma_2
+print(f"2. La suma de los {n} números es: {suma}")
+#Condicinal dentro del format
+print(f"La suma es: {suma} -> {'Par' if suma%2 == 0 else 'Impar'}")
+#forma_3
+print("3. La suma de los {} números es: {}".format(n,suma))
